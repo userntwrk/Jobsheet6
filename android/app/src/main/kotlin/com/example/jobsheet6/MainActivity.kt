230fe7c47539f857644decde4f0cd683b8909db3
wahyu <userblank21@gmail.com>
2022-05-28 15:13:30 +0700

    DBSQLite

---
+++ android/app/src/main/kotlin/com/example/jobsheet6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jobsheet6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+//kode utama Aplikasi tampilan awal
+import 'package:flutter/material.dart';
+import 'Page/Home.dart';
+
+//package letak folder Anda
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tambahkan Item',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Home(),
+    );
+  }
+}
